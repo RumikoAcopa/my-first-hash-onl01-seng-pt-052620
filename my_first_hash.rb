54e8c5aaf88018d9abfadde0d97353a2bd337f5c
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 19:23:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,19 +7,7 @@ def shipping_manifest
   "oil paintings" => 3}
 
 end
-
-
-  # use the literal
-
-
-  #constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-
-end
-
-
-def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
+# set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
