13fc782af696930c6b9e3ca7e92cfc7321edf7c6
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 19:20:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,12 @@
 def my_hash
   your_hash = {"summer" => "pool time", "friends" => "drinks"}
+end
 
 def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2
   "oil paintings" => 3}
+
+end  
 
 
   # use the literal
